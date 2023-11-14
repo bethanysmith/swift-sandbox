de265d2e90ec38ae6c799aa1c0c10aecaa2f8803
Bethany Smith <bethanys1609@gmail.com>
2023-11-14 21:03:04 +1100

    Load list of posts to demonstrate data injection

---
+++ SwiftSandbox/SwiftSandbox/ProtocolsSandbox.swift
@@ -0,0 +1,8 @@
+//
+//  ProtocolsSandbox.swift
+//  SwiftSandbox
+//
+//  Created by Bethany Smith on 14/11/2023.
+//
+
+import Foundation
--- SwiftSandbox/SwiftSandbox/SwiftSandboxApp.swift
+++ SwiftSandbox/SwiftSandbox/SwiftSandboxApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftSandboxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DependencyInjectionView(dataService: UrlDataService(url: nil))
         }
     }
 }
