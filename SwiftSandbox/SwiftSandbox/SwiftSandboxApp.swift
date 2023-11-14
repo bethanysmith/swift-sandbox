2499d6bf41af36fb359e8a62ba38395f60327072
Bethany Smith <bethanys1609@gmail.com>
2023-11-14 21:02:48 +1100

    Initial project commit

---
+++ SwiftSandbox/SwiftSandbox/SwiftSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSandboxApp.swift
+//  SwiftSandbox
+//
+//  Created by Bethany Smith on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
